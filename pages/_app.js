44a4e247bbaeca78045ef9c5d3c141726155c1d3
Bojan Gabric <bojan@bojangabric.com>
2020-08-15 01:47:25 +0200

    Add seo to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import Header from '../components/header';
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
 import '../css/tailwind.css';
 
 const App = ({ Component, pageProps }) => (
-  <div className="px-4 sm:px-6 md:px-0 my-20 mx-auto max-w-2xl font-serif">
+  <div className="px-4 sm:px-6 md:px-0 my-20 mx-auto max-w-2xl font-serif text-gray-900">
+    <DefaultSeo {...SEO} />
     <Header />
     <Component {...pageProps} />
   </div>
