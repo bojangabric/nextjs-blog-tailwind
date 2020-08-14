33b6fbdbe4c890553382b25063746fa2a112ec25
Bojan Gabric <bojan@bojangabric.com>
2020-08-15 01:47:42 +0200

    Update blog index page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,17 +1,27 @@
 import Link from 'next/link';
+import Seo from '../../components/seo';
 import { frontMatter } from './**/*.mdx';
+import moment from 'moment';
 
 const formatPath = p => p.replace(/\/index.mdx$/, '');
 
 const BlogPage = () => (
   <>
-    <h1 className="text-xl font-bold mb-4">All Blog Posts</h1>
-    <ul className="list-inside list-disc">
+    <Seo
+      title={'Blog'}
+      description={'Get awesome free content that helps you improve your design and web development skills.'}
+      ogImageUrl={'/images/photo.png'}
+    />
+    <ul className="space-y-6">
       {frontMatter.map(page => (
         <li key={page.__resourcePath}>
-          <Link href={formatPath(page.__resourcePath)}>
-            <a className="hover:underline">{page.title}</a>
-          </Link>
+          <div className="md:flex justify-between items-start mb-2">
+            <Link href={formatPath(page.__resourcePath)}>
+              <a className="hover:underline font-semibold text-xl md:pr-10">{page.title}</a>
+            </Link>
+            <div className="text-gray-700 whitespace-no-wrap">{moment(page.published).format('MMMM Do YYYY')}</div>
+          </div>
+          <div className="text-gray-800">{page.description}</div>
         </li>
       ))}
     </ul>
