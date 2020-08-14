d97b0142e46fb98272b593948a00a2f288302a9f
Bojan Gabric <bojan@bojangabric.com>
2020-08-15 01:47:35 +0200

    Update post layout

--- layouts/index.js
+++ layouts/index.js
@@ -1,8 +1,15 @@
+import Seo from '../components/seo';
+import moment from 'moment';
+
 const Layout = frontMatter => ({ children: content }) => (
-  <div className="prose">
-    <h1>{frontMatter.title}</h1>
-    {content}
-  </div>
+  <>
+    <Seo title={frontMatter.title} description={frontMatter.description} ogImageUrl={frontMatter.ogImage} />
+    <div>
+      <div className="text-lg text-gray-700">{moment(frontMatter.published).format('MMMM Do YYYY')}</div>
+      <h1 className="font-sans font-bold text-3xl lg:text-5xl leading-tight">{frontMatter.title}</h1>
+      <div className="markdown">{content}</div>
+    </div>
+  </>
 );
 
 export default Layout;
