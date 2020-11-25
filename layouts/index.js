c6b9605b0e11bd33d41e9ddbf07ded706798a25a
Bojan Gabric <bojan@bojangabric.com>
2020-11-25 16:33:36 +0100

    Update post layout

--- layouts/index.js
+++ layouts/index.js
@@ -1,15 +1,22 @@
 import Seo from '../components/seo';
-import moment from 'moment';
+import { useRouter } from 'next/router';
 
-const Layout = frontMatter => ({ children: content }) => (
-  <>
-    <Seo title={frontMatter.title} description={frontMatter.description} ogImageUrl={frontMatter.ogImage} />
-    <div>
-      <div className="text-lg text-gray-700">{moment(frontMatter.published).format('MMMM Do YYYY')}</div>
-      <h1 className="font-sans font-bold text-3xl lg:text-5xl leading-tight">{frontMatter.title}</h1>
-      <div className="markdown">{content}</div>
-    </div>
-  </>
-);
+const Layout = ({ children, frontMatter }) => {
+  const router = useRouter();
+
+  return (
+    <>
+      <Seo
+        title={frontMatter.title}
+        description={frontMatter.description}
+        canonical={`https://bojangabric.com${router.pathname}`}
+      />
+      <div>
+        <h1 className="font-sans font-bold text-3xl lg:text-5xl leading-tight text-center">{frontMatter.title}</h1>
+        <div className="markdown">{children}</div>
+      </div>
+    </>
+  );
+};
 
 export default Layout;
