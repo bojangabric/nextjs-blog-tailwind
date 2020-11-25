5e8e03b779ff5f661a49fa128a705336ad94bad9
Bojan Gabric <bojan@bojangabric.com>
2020-11-25 16:33:24 +0100

    Update BlogPage

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Seo from '../../components/seo';
+import { format } from 'date-fns';
 import { frontMatter } from './**/*.mdx';
-import moment from 'moment';
 
 const formatPath = p => p.replace(/\/index.mdx$/, '');
 
@@ -9,19 +9,25 @@ const BlogPage = () => (
   <>
     <Seo
       title={'Blog'}
-      description={'Get awesome free content that helps you improve your design and web development skills.'}
-      ogImageUrl={'/images/photo.png'}
+      description={'Awesome free content that helps you improve your design and web development skills.'}
+      canonical={'https://bojangabric.com/blog'}
     />
-    <ul className="space-y-6">
-      {frontMatter.map(page => (
+    <h1 className="font-sans font-bold text-3xl lg:text-5xl leading-tight mb-6 md:mb-16">Blog</h1>
+
+    <ul className="space-y-10">
+      {frontMatter.reverse().map(page => (
         <li key={page.__resourcePath}>
-          <div className="md:flex justify-between items-start mb-2">
-            <Link href={formatPath(page.__resourcePath)}>
-              <a className="hover:underline font-semibold text-xl md:pr-10 font-sans">{page.title}</a>
-            </Link>
-            <div className="text-gray-700 whitespace-no-wrap">{moment(page.published).format('MMMM Do YYYY')}</div>
+          <div className="md:flex justify-between mb-2">
+            <h2 className="md:mr-10">
+              <Link href={`/${formatPath(page.__resourcePath)}`}>
+                <a className="font-semibold text-xl leading-snug hover:underline font-sans">{page.title}</a>
+              </Link>
+            </h2>
+            <span className="text-gray-600 whitespace-nowrap text-sm mt-px">
+              {format(new Date(page.published), 'LLLL do, yyyy')}
+            </span>
           </div>
-          <div className="text-gray-800">{page.description}</div>
+          <p className="text-gray-800">{page.description}</p>
         </li>
       ))}
     </ul>
